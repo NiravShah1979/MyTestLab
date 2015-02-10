865c0d9154d3d4e9c995d973a48348e367343858
NiravShah1979 <sainirav@yahoo.com>
2015-02-10 16:19:43 +0000

    Updated Code

--- MyTestLab/MyTestWeb/Controllers/RestaurantController.cs
+++ MyTestLab/MyTestWeb/Controllers/RestaurantController.cs
@@ -8,11 +8,16 @@ namespace MyTestWeb.Controllers
     {
         public ActionResult Index()
         {
-            var menu = MenuItems();
-            
-            return View(menu);
+            var baseItem = new List<BaseItem>()
+            {
+                new BaseItem() { ItemType = ItemType.Starter }, 
+                new BaseItem() { ItemType = ItemType.Main }
+            };
+            baseItem[0].Item = StarterMenuItems();
+            baseItem[1].Item = MainsMenuItems();
+            return View(baseItem);
         }
-      
+
         public ActionResult Detail(string id)
         {
             var itemDetail = new ItemDetail
@@ -23,7 +28,39 @@ namespace MyTestWeb.Controllers
             return View(itemDetail);
         }
 
-        private List<Item> MenuItems()
+        [HttpGet]
+        public ActionResult Add()
+        {
+            var item = new Item
+            {
+                ItemTypes = BuildDropdown()
+            };
+            return View(item);
+        }
+
+
+        [ValidateInput(true)]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Update(Item item)
+        {
+            item.ItemTypes = BuildDropdown();
+            return View("Add", item);
+        }
+
+        public ActionResult Mains(string id)
+        {
+            var mains = MainsMenuItems();
+            return PartialView("_Mains", mains);
+        }
+
+        public PartialViewResult Starters(string id)
+        {
+            var starters = StarterMenuItems();
+            return PartialView("_Starters", starters);
+        }
+
+        private List<Item> StarterMenuItems()
         {
             var menuItems = new List<Item>()
             {
@@ -31,30 +68,65 @@ namespace MyTestWeb.Controllers
                 {
                     ItemCode = "ST001",
                     ItemName = "Veg Samosa",
+                    ItemTypes = new List<SelectListItem> { new SelectListItem() { Text = ItemType.Starter.ToString(), Value = ItemType.Starter.ToString() } },
                     Price = "2.99",
                 },
                 new Item()
                 {
                     ItemCode = "ST002",
                     ItemName = "Chilli Mogo",
+                    ItemTypes = new List<SelectListItem> { new SelectListItem() { Text = ItemType.Starter.ToString(), Value = ItemType.Starter.ToString() } },
                     Price = "4.99",
                 },
             };
             return menuItems;
         }
-        
-        [HttpGet]
-        public ActionResult Add()
+
+        private List<Item> MainsMenuItems()
         {
-            var item = new Item();
-            return View(item);
+            var menuItems = new List<Item>()
+            {
+                new Item()
+                {
+                    ItemCode = "MI001",
+                    ItemName = "Veg Kofta",
+                    ItemTypes = new List<SelectListItem> { new SelectListItem() { Text = ItemType.Main.ToString(), Value = ItemType.Main.ToString() } },
+                    Price = "5.99",
+                },
+                new Item()
+                {
+                    ItemCode = "MI002",
+                    ItemName = "Paneer Butter Masala",
+                    ItemTypes = new List<SelectListItem> { new SelectListItem() { Text = ItemType.Main.ToString(), Value = ItemType.Main.ToString() } },
+                    Price = "6.99",
+                },
+            };
+            return menuItems;
         }
 
-
-        [HttpPost]
-        public ActionResult Update(Item formCollection)
+        private List<SelectListItem> BuildDropdown()
         {
-            return View("Add", formCollection);
+            return new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                    Selected = false,
+                    Text = ItemType.Starter.ToString(),
+                    Value = ItemType.Starter.ToString()
+                },
+                new SelectListItem()
+                {
+                    Selected = false,
+                    Text = ItemType.Main.ToString(),
+                    Value = ItemType.Main.ToString()
+                },
+                new SelectListItem()
+                {
+                    Selected = false,
+                    Text = ItemType.Desserts.ToString(),
+                    Value = ItemType.Desserts.ToString()
+                }
+            };
         }
     }
 }
--- MyTestLab/MyTestWeb/Models/Item.cs
+++ MyTestLab/MyTestWeb/Models/Item.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MyTestWeb.Models
 {
-    public class Item
+    public class Item 
     {
         public string ItemCode { get; set; }
 
+        [Required(ErrorMessage = "{0} is required")]
         [Display(Name = "Name")]
         public string ItemName { get; set; }
 
+        public IEnumerable<SelectListItem> ItemTypes { get; set; }
+
+        public string SelectedItemType { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
         [DataType(DataType.Currency)]
         public string Price { get; set; }
     }
